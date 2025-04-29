d44bc3aa3ea9068014fd7956d83d5c5bd5449bff
sheepdog9678 <sheepdog13@naver.com>
2025-04-30 03:05:36 +0900

    feat : Header 컴포넌트

---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+import MenuIcon from "@mui/icons-material/Menu";
+import PersonIcon from "@mui/icons-material/Person";
+
+function Header() {
+  return (
+    <header className="w-full h-[50px] flex items-center justify-between bg-white p-3">
+      <button>
+        <MenuIcon className="w-6 h-6" />
+      </button>
+      <button>
+        <h1 className="text-2xl font-bold">심테</h1>
+      </button>
+      <button>
+        <PersonIcon className="w-6 h-6" />
+      </button>
+    </header>
+  );
+}
+
+export default Header;
